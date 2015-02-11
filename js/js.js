4cdea2ed115f420cbd0e9bea0b4f05e4034c7f07
marco <mrc_ant_18@hotmail.com>
2015-02-11 15:45:13 -0600

    efecto en ventana modal

--- js/js.js
+++ js/js.js
@@ -1,7 +1,15 @@
+function get_element(e){
+    var elem = document.getElementById(e);
+    return elem
+}
+
 function show_modal(){
-    document.getElementById("modal").style.display = "block"
+    get_element('modal').style.display = "block";
+    get_element('e-modal').style.display = "block";
 }
 
 function close_modal(){
-    document.getElementById("modal").style.display = "none"
+    get_element('modal').style.display = "none";
+    get_element('e-modal').style.display = 'none'
 }
+
