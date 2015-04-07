e7adf89f2304cee1420c20b04ea203721b2db176
Marco A. Vazquez A <mrc_ant_18@hotmail.com>
2015-04-07 00:04:01 -0500

    agregando nib y cambiando fondo

--- js/js.js
+++ js/js.js
@@ -1,16 +1,4 @@
- $("#btn-submit").click(function(){
-    $("#btn-submit").attr("disabled", "disabled");
-    var url = "https://agile-falls-8838.herokuapp.com/send"; // El script a dónde se realizará la petición.
-    $.ajax({
-           type: "POST",
-           url: url,
-           data: $("#form-contacto").serialize(), // Adjuntar los campos del formulario enviado.
-           success: function(data)
-           {
-                $("#form-contacto").hide();
-                $("#respuesta").html(data); // Mostrar la respuestas del script PHP.
-           }
-         });
-
-    return false; // Evitar ejecutar el submit del formulario.
- });
+$('nav a').click(function(e){
+  e.preventDefault();
+  $('html, body').stop().animate({scrollTop: $($(this).attr('href')).offset().top}, 1000);
+});
